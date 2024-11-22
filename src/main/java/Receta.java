a2d13d1ef2c23677c15b7f4b4941694350aa295d
alvar <alvarotamaki1526@gmail.com>
2024-11-22 12:55:22 +0100

    Te quiero brodi

--- src/main/java/Receta.java
+++ src/main/java/Receta.java
@@ -5,6 +5,7 @@ public class Receta {
     private int maxIngredientes;
     private int maxInstrucciones;
     private int numIngredientes=0;//A lo mejor no hace falta crear esta variable
+    private int numInstrucciones=0;
     public Receta(String nombre, int maxIngredientes, int maxInstrucciones) {
         // Inicialización de la receta
         this.nombre= nombre;
@@ -29,9 +30,14 @@ public class Receta {
         return Instrucciones;
     }
 
+    /**
+     * Funcion que añade un Ingrediente a la receta y un boolean 0/1 depende si se ha llegado al limite de ingredientes.
+     * @param ingrediente - String del nombre del ingrediente que se quiere añadir a la receta.
+     * @return
+     */
     public boolean agregarIngrediente(String ingrediente) {
         // Añade un ingrediente a la receta
-        if(numIngredientes>maxIngredientes)
+        if(ingredientesCompletos())
             return false;
         else {
             Ingredientes[numIngredientes] = ingrediente;
@@ -42,49 +48,74 @@ public class Receta {
     }
 
     public boolean agregarInstruccion(String instruccion) {
-        // Añade una instrucción a la receta
-        return false; // @todo MODIFICAR PARA DEVOLVER SI SE HA AÑADIDO LA INSTRUCCIÓN
+        if(instruccionesCompletas())
+            return false;
+        else {
+            Instrucciones[numInstrucciones] = instruccion;
+            numInstrucciones++;
+
+            return false; // @todo MODIFICAR PARA DEVOLVER SI SE HA AÑADIDO LA INSTRUCCIÓN
+        }
     }
 
     public boolean ingredientesCompletos() {
         // Comprueba si la receta tiene el máximo de ingredientes
-        return false; // @todo MODIFICAR PARA DEVOLVER SI ESTÁN COMPLETOS LOS INGREDIENTES
+        if (numIngredientes>(maxIngredientes-1))
+        return true; // @todo MODIFICAR PARA DEVOLVER SI ESTÁN COMPLETOS LOS INGREDIENTES
+        else
+            return false;
     }
 
     public boolean instruccionesCompletas() {
         // Comprueba si la receta tiene el máximo de instrucciones
+        if(numInstrucciones>(maxInstrucciones-1))
+            return true;
+        else
         return false; // @todo MODIFICAR PARA DEVOLVER SI ESTÁN COMPLETAS LAS INSTRUCCIONES
     }
 
     public int numIngredientes() {
-        // Devuelve el número de ingredientes de la receta
-        return 0; // @todo MODIFICAR PARA DEVOLVER EL NÚMERO DE INGREDIENTES
+        return numIngredientes;
     }
 
     public int numInstrucciones() {
-        // Devuelve el número de instrucciones de la receta
-        return 0; // @todo MODIFICAR PARA DEVOLVER EL NÚMERO DE INSTRUCCIONES
+       return numInstrucciones;
     }
 
     @Override
     public String toString() {
         // Devuelve una representación en forma de cadena de la receta
+        System.out.println("Receta: "+nombre);
+        System.out.println("Ingredientes:");
+        for(int i=0;i<=numIngredientes;i++) {
+            System.out.println(Ingredientes[i]);
+        }
+        System.out.println("Instrucciones:");
+        for(int i=0;i<=numInstrucciones;i++){
+            System.out.println((i+1)+". "+Instrucciones[i]);
+        }
         return null; // @todo MODIFICAR PARA DEVOLVER LA CADENA CORRECTA
     }
 
     public String toRawString() {
-        // Devuelve una representación en forma de cadena de la receta sin formato
+        System.out.println(nombre);
+        System.out.println("INGREDIENTES");
+        for(int i=0;i<=numIngredientes;i++) {
+            System.out.println(Ingredientes[i]);
+        }
+        System.out.println("INSTRUCCIONES");
+        for(int i=0;i<=numInstrucciones;i++){
+            System.out.println(Instrucciones[i]);
+        }
         return null; // @todo MODIFICAR PARA DEVOLVER LA CADENA CORRECTA
     }
 
     public int getMaxIngredientes() {
-        // Devuelve el máximo de ingredientes permitidos en la receta
-        return 0; // @todo MODIFICAR PARA DEVOLVER EL MÁXIMO DE INGREDIENTES
+       return maxIngredientes;
     }
 
     public int getMaxInstrucciones() {
-        // Devuelve el máximo de instrucciones permitidas en la receta
-        return 0; // @todo MODIFICAR PARA DEVOLVER EL MÁXIMO DE INSTRUCCIONES
+        return maxInstrucciones;
     }
 }
 
