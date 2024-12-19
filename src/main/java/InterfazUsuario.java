acca2e65792feb69c9d706117da6f34cd8f5ecde
alvar <alvarotamaki1526@gmail.com>
2024-12-19 14:07:01 +0100

    InterfazUsuario finiquitada

--- src/main/java/InterfazUsuario.java
+++ src/main/java/InterfazUsuario.java
@@ -3,7 +3,12 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 /**
- *Clase la cual se encarga de
+ *Clase la cual se encarga de las interacciones con el usuario.
+ *Esta clase se inicializa con parámetros definidos en la linea de comandos al ejecutarse el programa
+ *Contiene la función que crea el menú legible, el cual tiene numerosas funciones: Te permite agregar una receta,
+ * Consultar, editar o borrar una receta que elija el ususario, Editar el planificador semanal, guardar un
+ * archivo con todas las recetas y cargar un archivo que añade las recetas. Además puede guardar el Plan Semanal
+ * en un archivo de texto para su lectura y uso.
  */
 public class InterfazUsuario {
     private LibroDeRecetas libroDeRecetas;
@@ -13,21 +18,30 @@ public class InterfazUsuario {
 
     /**
      * Función que incializa los parametros de la clase que recibe en la linea de comandos al ejecutarse el programa.
-     *Además inicializa el Libro de Recetas y el Planificador Semanal
+     *Además inicializa el Libro de Recetas.
      * @param maxIngredientes Cantidad máxima de ingredientes que puede tener cada receta.(int)
      * @param maxInstrucciones Cantidad máxima de istrucciones que puede tener cada receta.(int)
      * @param maxRecetasEnLibro Cantidad máxima de recetas que puede guardar el Libro de Recetas.(int)
      */
     public InterfazUsuario(int maxIngredientes, int maxInstrucciones, int maxRecetasEnLibro) {
-        // Inicialización de la herramienta de recetas
         libroDeRecetas=new LibroDeRecetas(maxRecetasEnLibro);
         this.maxIngredientes=maxIngredientes;
         this.maxInstrucciones=maxInstrucciones;
-        planificador=new PlanificadorSemanal();
     }
 
+    /**
+     * Función que incializa los parametros de la clase que recibe en la linea de comandos al ejecutarse el programa,
+     * la cual contiene el nombre de un archivo desde el que cargar sus recetas.
+     * Además inicializa el Libro de Recetas.
+     * @param maxIngredientes Cantidad máxima de ingredientes que puede tener cada receta.(int)
+     * @param maxInstrucciones Cantidad máxima de istrucciones que puede tener cada receta.(int)
+     * @param maxRecetasEnLibro Cantidad máxima de recetas que puede guardar el Libro de Recetas.(int)
+     * @param archivoRecetas Nombre del archivo del cual se cargan las recetas.(String)
+     */
     public InterfazUsuario(int maxIngredientes, int maxInstrucciones, int maxRecetasEnLibro, String archivoRecetas) {
-        this(maxIngredientes, maxInstrucciones, maxRecetasEnLibro);
+        libroDeRecetas=new LibroDeRecetas(maxRecetasEnLibro);
+        this.maxIngredientes=maxIngredientes;
+        this.maxInstrucciones=maxInstrucciones;
         try {
             libroDeRecetas.cargarRecetasDeArchivo(archivoRecetas, maxIngredientes, maxInstrucciones);
         } catch (IOException e) {
@@ -37,14 +51,22 @@ public class InterfazUsuario {
         // Cargar las recetas predefinidas al iniciar la aplicación
     }
 
+    /**
+     * Función que inicializa el Scanner, la clase del Planificador semanal y ejecuta la función del menú principal.
+     */
     public void iniciar() {
         Scanner scanner = new Scanner(System.in);
+        planificador=new PlanificadorSemanal();
         menuPrincipal(scanner);
         scanner.close();
     }
 
+    /**
+     * Función que se encarga de imprimir el menú principal y recibiendo la entrada del usuario invoca la función
+     * deseada.
+     * @param scanner Scanner, el cual se utiliza en el todas las funciones que ejecuta esta función.
+     */
     private void menuPrincipal(Scanner scanner) {
-        // Muestra el menú principal y gestiona la entrada del usuario para dirigirlo a la opción seleccionada
         int opcion;
         do {
             System.out.println("--- Menú Principal ---");
@@ -82,8 +104,12 @@ public class InterfazUsuario {
         }while(opcion!=7);
     }
 
+    /**
+     * Función que recibe la entrada del usuario para crear una receta. Esto incluye un nombre, ingredientes e
+     * instrucciones elegidas por el usuario.
+     * @param scanner Scanner necesario para ejecutar las funciones.
+     */
     private void agregarReceta(Scanner scanner) {
-        // Solicita al usuario los datos de la receta y la añade al libro de recetas
         Receta receta=new Receta(Utilidades.leerCadena(scanner,"Nombre de la receta: "),maxIngredientes,maxInstrucciones);
         System.out.println("Introduce los ingredientes (una línea por ingrediente, escribe 'fin' para terminar):");
         String ingredienteinstruccion;
@@ -98,6 +124,11 @@ public class InterfazUsuario {
             System.out.println("¡Receta agregada exitosamente!");
     }
 
+    /**
+     * Función que ejecuta la función que busca recetas según un String que escribe el usuario, una vez elegida la
+     * receta, la imprime y despues invoca la función que permite al usuario editar la receta.
+     * @param scanner Scanner necesario para ejecutar las funciones.
+     */
     private void consultarReceta(Scanner scanner) {
         // Busca una receta por su nombre y activa el menú de edición
        Receta receta= buscarRecetaPorNombre(scanner);
@@ -105,11 +136,19 @@ public class InterfazUsuario {
            System.out.println(receta.toString());
            editarReceta(scanner, receta);
        }
-        }
+    }
 
 
+    /**
+     * Función busca recetas cuyos nombres coincidan con la entrada del usuario(un String), después envia el array de
+     * las recetas encontradas a la función seleccionarReceta, la cual permite al usuario escoger una receta la cual
+     * será lo que devuelve la función original.
+     * @param scanner Scanner necesario para ejecutar las funciones.
+     * @return Devuelve una receta de la clase Receta, que coincide con el criterio de busqueda y es elegida por el
+     * usuario.
+     */
     private Receta buscarRecetaPorNombre(Scanner scanner) {
-        // Solicita al usuario un texto para buscar y seleccionar una receta por su nombre
+        Receta receta=null;
             String cadena = Utilidades.leerCadena(scanner, "Introduce el texto de la receta a buscar (-FIN- para volver): ");
             boolean recetasVacias=true;
             if (!(cadena).equalsIgnoreCase("FIN")) {
@@ -121,12 +160,18 @@ public class InterfazUsuario {
                 }
                 if(recetasVacias)buscarRecetaPorNombre(scanner);
                 else {
-                    return seleccionarReceta(scanner, recetasEncontradas);
+                    receta= seleccionarReceta(scanner, recetasEncontradas);
                 }
             }
-            return null;
+            return receta;
     }
 
+    /**
+     * Función que permite al susuario editar una receta seleccionada, con la posibilidad de añadir un ingredente,
+     * añadir una instrucción o borrar tda la receta.
+     * @param scanner Scanner necesario para ejecutar las funciones.
+     * @param seleccionada Receta que fue elegida por el usuario para ser editada.
+     */
     private void editarReceta(Scanner scanner, Receta seleccionada) {
         // Pantalla de edición de receta
         int opcion;
@@ -150,6 +195,13 @@ public class InterfazUsuario {
             }
     }
 
+    /**
+     * Función que recibe un array con las recetas cuyos nombres coinciden con la entrada del usuario, la cual imprime
+     * el nombre de todas esas recetas para que el usuario pueda elegir cual de ellas quiere editar.
+     * @param scanner Scanner necesario para ejecutar las funciones.
+     * @param recetas Array que contiene todas las recetas encontradas por la funcion buscarRecetaPorNombre
+     * @return Devuelve la receta elegida por el usuario.
+     */
     private Receta seleccionarReceta(Scanner scanner, Receta[] recetas) {
         // Muestra las recetas encontradas y solicita al usuario que elija una
         int numRecetasEncontradas=0;
@@ -164,6 +216,11 @@ public class InterfazUsuario {
         return recetas[(recetaElegida-1)];
     }
 
+    /**
+     * Función que permite al usuario editar el plan semanal, añadiendo comidas un dia de la semana o sustituyendo
+     * existentes comidas por otras.
+     * @param scanner Scanner necesario para ejecutar las funciones.
+     */
     private void planificarComidas(Scanner scanner) {
         // Inicia el proceso de planificación de comidas
         System.out.println("Planificación de comidas para la semana:");
@@ -174,6 +231,10 @@ public class InterfazUsuario {
         System.out.println("Receta planificada para "+Utilidades.posicionADiaSemana(intdia));
     }
 
+    /**
+     * Función que guarda la información de todas las recetas para que puedan ser cargadas más tarde.
+     * @param scanner Scanner necesario para ejecutar las funciones.
+     */
     private void guardarRecetas(Scanner scanner) {
         // Solicita al usuario un nombre de archivo y guarda las recetas en ese archivo
        String nombreArchivo=Utilidades.leerCadena(scanner, "Introduce el nombre del archivo donde guardar las recetas: ");
@@ -185,6 +246,11 @@ public class InterfazUsuario {
        }
     }
 
+    /**
+     * Función que lee un archivo de texto y carga las recetas que se encuentren en este y envia un mensaje de error
+     * si no se pudo cargar el archivo o si el archivo no existe.
+     * @param scanner Scanner necesario para ejecutar las funciones.
+     */
     private void cargarRecetas(Scanner scanner) {
         String nombreArchivo=Utilidades.leerCadena(scanner,"Introduce la ruta del archivo de donde cargar las recetas: ");
         try {
@@ -195,6 +261,12 @@ public class InterfazUsuario {
         }
         // Solicita al usuario un nombre de archivo y carga las recetas desde ese archivo
     }
+
+    /**
+     * Guarda el plan semanal en una archivo legible, para el uso propio del usuario.
+     * Devuelve un mensaje de error si no se pudo guardar el archivo.
+     * @param scanner Scanner necesario para ejecutar las funciones.
+     */
     private void guardarPlanSemanal(Scanner scanner) {
         // Solicita al usuario un nombre de archivo y guarda el plan semanal en ese archivo
         String nombreArchivo=Utilidades.leerCadena(scanner,"Introduce el nombre del archivo donde guardar el plan semanal: ");
